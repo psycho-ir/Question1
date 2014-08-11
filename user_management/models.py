75c6c89b9dbbfb17d92197b52cf4091b21b41191
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2014-08-11 20:49:07 +0430

    first user model designed

--- user_management/models.py
+++ user_management/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=50,primary_key=True)
+    password = models.CharField(max_length=50)
+    email = models.EmailField()
+
+
+    def set_password(self,plain_pass):
+        pass
