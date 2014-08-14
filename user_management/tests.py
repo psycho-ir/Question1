4a9e994b5faca85ef0626afb2025e50a7ee647f3
soroosh <soroosh.sarabadani@gmail.com>
2014-08-14 14:38:27 +0430

    tests improved

--- user_management/tests.py
+++ user_management/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 from user_management.models import User
-from user_management.user_manager import authenticate, CaptchaException, register_user
+from user_management.user_manager import authenticate, CaptchaException, register_user, activate
 
 password = 123456
+
 
 def persis_test_user():
     user = User()
@@ -30,9 +31,15 @@ class UserPersistantTest(TestCase):
         self.assertFalse(self.user.is_pass_correct(12345))
 
 
+class UserActivationException(object):
+    pass
+
+
 class UserManagementTest(TestCase):
     def setUp(self):
         self.user = persis_test_user()
+        self.user.active = True
+        self.user.save()
 
     def test_authenticate_shoud_return_User(self):
         self.assertEqual(authenticate('soroosh', password).email, self.user.email)
@@ -52,5 +59,11 @@ class UserManagementTest(TestCase):
         u.username = 'ali'
         u.set_password('123')
         u.email = 'ali@iran.ir'
+        u.active = True
         register_user(u, 'captcha', fake_session)
-        self.assertIsNotNone(authenticate('ali','123'))
+        self.assertIsNotNone(authenticate('ali', '123'))
+
+    def test_activate_should_raise_exception_if_user_is_active(self):
+        self.user.activation_code = '111'
+        self.user.save()
+        self.assertRaises(Exception, activate,'111')
